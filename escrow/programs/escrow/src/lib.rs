8169276861297dc7fc9b4537b9197d8984758261
Zeref <krishh16@yahoo.com>
2024-10-22 23:36:37 +0530

    added the escrow program end to end✅

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -3,23 +3,30 @@ use anchor_lang::prelude::*;
 pub mod states;
 pub mod instructions;
 
-use states::EscrowState;
 use instructions::{
     initialize_escrow::*,
-    
+    deposit::*,
+    withdraw::*
 };
 
 declare_id!("FS22fXE6AnE72uJo8ZUqcbDLNFVkbjKTgYrbaiCzBhL9");
 
 #[program]
 pub mod escrow {
+
     use super::*;
 
     pub fn initialize(ctx: Context<InitializeEscrow>) -> Result<()> {
         ctx.accounts.new(ctx.bumps)
     }
 
-    
+    pub fn deposit(ctx: Context<DepositAccounts>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)
+    }
+
+    pub fn withdraw(ctx: Context<WithdrawAccounts>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)
+    }
 }
 
 
