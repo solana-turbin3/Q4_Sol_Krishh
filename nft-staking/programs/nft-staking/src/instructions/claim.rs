28ee84d00795680e850a235a3ef7046a99e310ea
Zeref <krishh16@yahoo.com>
2024-10-31 00:42:33 +0530

    added the claim function and completed all teh instruction

--- nft-staking/programs/nft-staking/src/instructions/claim.rs
+++ nft-staking/programs/nft-staking/src/instructions/claim.rs
@@ -34,3 +34,29 @@ pub struct Claim<'info> {
     pub token_program: Program<'info, Token>,
     pub associated_token_program: Program<'info, AssociatedToken>
 }
+
+
+impl<'info> Claim<'info> {
+    pub fn claim(&mut self) -> Result<()> {
+        let cpi_program = self.token_program.to_account_info();
+
+        let seeds = &[
+            b"config".as_ref(),
+            &[self.config.bump]
+        ];     
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_accounts = MintTo {
+            mint: self.reward_mint.to_account_info(),
+            to: self.reward_ata.to_account_info(),
+            authority: self.config.to_account_info(),
+        };
+
+        let cpi_context = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        mint_to(cpi_context, self.user_account.point as u64 * 10_u64.pow(self.reward_mint.decimals as u32))?;
+
+        self.user_account.point = 0;
+        Ok(())
+    }
+}
