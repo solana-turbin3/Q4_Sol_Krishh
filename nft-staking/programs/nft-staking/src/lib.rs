3aaff776cb353d2b3eb3386f4a8c559bafa69fc0
Zeref <krishh16@yahoo.com>
2024-10-31 00:46:07 +0530

    finished the nft minter program

--- nft-staking/programs/nft-staking/src/lib.rs
+++ nft-staking/programs/nft-staking/src/lib.rs
@@ -9,7 +9,9 @@ pub mod errors;
 use instructions::{
     init_stake::*,
     init_stake_configs::*,
-    init_user_account::*
+    init_user_account::*,
+    unstake::*,
+    claim::*
 };
 
 #[program]
@@ -18,6 +20,22 @@ pub mod nft_staking {
 
     pub fn initialize_config(ctx: Context<InitStakeConfigs>, points_per_stake: u8, max_stake: u8, freeze_period: u32 ) -> Result<()> {
         ctx.accounts.init(&ctx.bumps, points_per_stake, max_stake, freeze_period)
+    }
+
+    pub fn init_acocunt(ctx: Context<InitUserAccount>) -> Result<()> {
+        ctx.accounts.init_account(&ctx.bumps)
+    }
+
+    pub fn init_stake_account(ctx: Context<InitStakeAccount>)-> Result<()> {
+        ctx.accounts.init_stake(&ctx.bumps)
+    }
+
+    pub fn unstake(ctx: Context<Unstake>) -> Result<()> {
+        ctx.accounts.unstake()
+    }
+
+    pub fn claim(ctx: Context<Claim>)-> Result<()> {
+        ctx.accounts.claim()
     }
 }
 
