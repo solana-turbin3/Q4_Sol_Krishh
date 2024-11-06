3af7645f29c161398c1f93504407f7992e91229e
Zeref <krishh16@yahoo.com>
2024-11-06 16:52:40 +0530

    added market place state and the boilerplate code'

---
+++ nft-marketplace/programs/nft-marketplace/src/lib.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+
+pub mod states;
+pub mod instrucitons;
+
+declare_id!("25HguGejmsSEDUDYrKd7WQf21jtvYDFcSRJkXSPqcS3u");
+
+#[program]
+pub mod nft_marketplace {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
---
+++ nft-marketplace/programs/nft-marketplace/src/states/marketplace.rs
@@ -0,0 +1,13 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct MarketPlace {
+    pub reward_bump: u8,
+    pub fee: u16,
+    pub admin: Pubkey,
+    pub bump: u8,
+    pub treasury_bump: u8,
+    #[max_len(32)]
+    pub name:  String
+}
