7da6b5cc702bc5a5a47e03bf0ac023d77b650936
Zeref <krishh16@yahoo.com>
2024-11-13 15:04:23 +0530

    added the config states

---
+++ amm/programs/amm/src/lib.rs
@@ -0,0 +1,21 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9zmQYSuFqX5DeFkR7K6D4W2HaKWvkPmaGrVrgMTQqFDw");
+
+pub mod instructions;
+pub mod states;
+pub mod constants;
+pub mod errors;
+
+#[program]
+pub mod amm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
---
+++ amm/programs/amm/src/states/config.rs
@@ -0,0 +1,19 @@
+use anchor_lang::prelude::*;
+use crate::constants::*;
+
+#[account]
+pub struct Configs {
+    pub seeds: u64, //Donnno what this is yet
+    pub mint_a: Pubkey,
+    pub mint_b: Pubkey,
+    pub config_bump: u8,
+    pub auth_bump: u8,
+    pub fee: u16,
+    pub locked: bool,
+    pub authority: Option<Pubkey>
+}
+
+impl Configs {
+    // Using variables so that there are no magic numbers 
+    pub const LEN: usize = 3 * PUBKEY_L + 1 * U16_L + 1 * BOOL_L + 1 * OPTION_L + 2 * U8_L + 8;
+}
